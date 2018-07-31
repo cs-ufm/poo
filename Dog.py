15a70bdaf1080e9d0ff28654bbfebad8385e2e52
Marcos Cano <jmarcos.cano@gmail.com>
2018-07-31 00:31:54 -0600

    @jmarcos-cano - Adds inheritance

--- Dog.py
+++ Dog.py
@@ -1,13 +1,30 @@
 class Dog():
     # class attribute
-    breed="chihuahua"
+    animal_group="mammal"
+    breed="Street Dog"
+
     # Constructor
     def __init__(self, name=None, age=0):
         # Instance attributes
         self.name = name
         self.age = age
 
+    # instance methods
+    def tag(self):
+        return "{} is {} years old".format(self.name, self.age)
 
+    def noise(self,sound):
+        return "{} says: {}".format(self.name,sound)
+
+    def sleep(self):
+        pass
+
+
+# inheritance
+class Corgi(Dog):
+    breed="Corgi"
+    def sleep(self):
+        return '{} is sleeping zZZZz'.format(self.name)
 
 
 
@@ -21,11 +38,20 @@ doggy=Dog("Gino",12)
 doge=Dog("Blacky",1)
 
 print(doggy.name)
-print(doggy.breed)
+print(doggy.age)
 
 print(doge.name)
-print(doge.breed)
+print(doge.age)
 
 
 print("{} is {} and {} is {}.".format(
     doggy.name, doggy.age, doge.name, doge.age))
+
+
+print("{} is a {}".format(doggy.name,doggy.animal_group))
+
+# inheritance
+terry=Corgi("Terry",2)
+print(terry.sleep())
+
+print(isinstance(terry,Corgi))
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # poo
+
+
+# Exercise:
+
+1. Create a Pets class that holds instances of dogs; this class is completely separate from the Dog class. In other words, the Dog class does not inherit from the Pets class. Then assign three dog instances to an instance of the Pets class. Start with the following code below. Save the file as pets_class.py. Your output should look like this:
+
+```
+I have 3 dogs.
+Tom is 6.
+Fletcher is 7.
+Larry is 9.
+And they're all mammals, of course.
+```
+
+
+2. Using the same file, add an instance attribute of is_hungry = True to the Dog class. Then add a method called eat() which changes the value of is_hungry to False when called. Figure out the best way to feed each dog and then output “My dogs are hungry.” if all are hungry or “My dogs are not hungry.” if all are not hungry. The final output should look like this:
+
+```
+I have 3 dogs.
+Tom is 6.
+Fletcher is 7.
+Larry is 9.
+And they're all mammals, of course.
+My dogs are not hungry.```
